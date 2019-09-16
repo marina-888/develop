7176318a6346260f7a79b09ce73c4b5b0c9410fa
marina-888 <ririri.888.am@gmail.com>
2019-09-16 13:58:25 +0900

    新規作成　NekoMangement

---
+++ app/src/main/java/com/example/marina/nekomanegement/NekoMangement.java
@@ -0,0 +1,14 @@
+package com.example.marina.nekomanegement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NekoMangement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neko_mangement);
+
+    }
+}
